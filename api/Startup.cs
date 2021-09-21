ae9148e73516ed9036ce60a290773be8cb822c6e
Zia Uddin Ahmed <zia.ete10@gmail.com>
2021-09-22 00:45:17 +0600

    exception handelling middleware

--- api/Startup.cs
+++ api/Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Extensions;
 using API.Interface;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -37,6 +38,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            
             // extionsiois to clean code
            services.AddApplicationServices(_config);
 
@@ -54,12 +57,14 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+
+            app.UseMiddleware<ExceptionMiddleware>();
+            /*if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "api v1"));
-            }
+            }*/
 
             app.UseHttpsRedirection();
 
