9b1525c8e61a38efb454e32e26e34dd5e22a436d
Zia Uddin Ahmed <zia.ete10@gmail.com>
2021-10-03 00:42:25 +0600

    ngx spinner store state so don't have to call api all time

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -3,6 +3,7 @@ import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Member } from '../_model/member';
 import { map } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 // const httpOptions ={
 //   headers : new HttpHeaders({
@@ -18,12 +19,21 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers()  {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if(this.members.length>0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members=>{
+        this.members = members;
+        return members;
+      })
+    );
   }
 
   getMember(username: string) {
+    const member = this.members.find(x => x.username === username);
+    if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
+  
   updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member).pipe(
       map(() => {
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -23,6 +23,9 @@ import { ServerErrorComponent } from './errors/server-error/server-error.compone
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './_interceptors/loading.interceptor';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -46,11 +49,16 @@ import { MemberEditComponent } from './members/member-edit/member-edit.component
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-    SharedModule
+    SharedModule,
+    NgxSpinnerModule,
+    RouterModule
+
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
     {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true},
+
     ],
   bootstrap: [AppComponent]
 })
