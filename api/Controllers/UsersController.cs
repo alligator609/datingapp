5a12b44434b06da048ab723ba0a66635c6aca17d
Zia Uddin Ahmed <zia.ete10@gmail.com>
2021-10-15 22:38:27 +0600

    pagination helper extension for api

--- api/Controllers/UsersController.cs
+++ api/Controllers/UsersController.cs
@@ -13,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http;
 using API.Extensions;
+using API.Helpers;
 
 namespace API.Controllers
 {
@@ -32,9 +33,10 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _userRepository.GetMembersAsync(); 
+            var users = await _userRepository.GetMembersAsync(userParams);
+            Response.AddPaginationHeader(users.CurrentPage, users.CurrentPage, users.TotalCount, users.TotalPages);
             return Ok(users);
         }
 
