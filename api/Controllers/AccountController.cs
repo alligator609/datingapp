ce2b85f92706ab1b45e3ad343418524fee88fae1
Zia Uddin Ahmed <zia.ete10@gmail.com>
2021-10-29 10:58:38 +0600

    added identity table and auth

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using API.DTOs;
 using API.Entities;
 using API.Interface;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,15 +15,17 @@ namespace Controllers
 {
     public class AccountController : BaseApiController
     {
-        private readonly DataContext _context;
-          private readonly ITokenService _tokenService;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
 
-        public AccountController(DataContext context,ITokenService tokenService,IMapper mapper)
+        public AccountController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager,ITokenService tokenService,IMapper mapper)
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
             _tokenService =tokenService;
             _mapper = mapper;
-            _context = context;
         }
 
 
@@ -34,14 +37,10 @@ namespace Controllers
 
             var user = _mapper.Map<AppUser>(registerDto);
 
-            using var hmac = new HMACSHA512();
-
             user.UserName = registerDto.UserName.ToLower();
-            user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-            user.PasswordSalt = hmac.Key;
 
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded) return BadRequest(result.Errors);
 
             return new UserDto
             {
@@ -55,14 +54,13 @@ namespace Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
 
-            var user = await _context.Users.Include(p => p.Photos).FirstOrDefaultAsync(x => x.UserName == loginDto.UserName);
+            var user = await _userManager.Users.Include(p => p.Photos).FirstOrDefaultAsync(x => x.UserName == loginDto.UserName.ToLower());
             if(user == null) return Unauthorized("user not found");
 
-            var hmac = new HMACSHA512(user.PasswordSalt);
-            var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-            for(int i =0 ; i< computeHash.Length ; i++){
-                if (computeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
-            }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized();
+
             return new UserDto{
             UserName = user.UserName,
             Token = _tokenService.CreateToken(user),
@@ -74,7 +72,7 @@ namespace Controllers
 
         private async Task<bool> UserExists (string username){
 
-            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+            return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
--- api/Data/DataContext.cs
+++ api/Data/DataContext.cs
@@ -1,17 +1,18 @@
 using System;
 using System.Threading.Tasks;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser,AppRole,int,IdentityUserClaim<int>,AppUserRole,IdentityUserLogin<int>, IdentityRoleClaim<int>,IdentityUserToken<int>>  
     {
         public DataContext( DbContextOptions<DataContext> options) : base(options)
-        {
+        { 
         }
 
-        public DbSet<AppUser> Users { get; set; }
 
         public DbSet<UserLike> Likes { get; set; }
         public DbSet<Message> Messages { get; set; }
@@ -20,7 +21,18 @@ namespace API.Data
         {
             base.OnModelCreating(builder); // to avoid migration error 
 
+            builder.Entity<AppUser>()
+               .HasMany(ur => ur.UserRoles)
+               .WithOne(u => u.User)
+               .HasForeignKey(ur => ur.UserId)
+               .IsRequired();
 
+            builder.Entity<AppRole>()
+                .HasMany(ur => ur.UserRoles)
+                .WithOne(u => u.Role)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
+             
             builder.Entity<UserLike>().HasKey(k => new { k.SourceUserId, k.LikedUserId });
 
             builder.Entity<UserLike>().HasOne(s => s.SourceUser).WithMany(l => l.LikedUsers).HasForeignKey(s => s.SourceUserId).OnDelete(DeleteBehavior.Cascade); //change to no action onsql server
